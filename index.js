ef84cf5c867f724098ca9325bda8f30a50c89345
William Toro <68400039+WillToro94@users.noreply.github.com>
2022-04-21 16:38:42 -0400

    Added Initial files

---
+++ index.js
@@ -0,0 +1,26 @@
+const randomNumber1 = Math.floor(Math.random()* 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = " Victory Player 1!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Victory Player 2! ";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
